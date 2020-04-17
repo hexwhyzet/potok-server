f4f83d1dc4d036f34326038d957ad3a01648f676
Alex <alexnossofff@gmail.com>
2020-04-17 23:07:35 +0300

    Picture shower not working

--- void/void_app/views.py
+++ void/void_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Meme
+from django.http import HttpResponse
 
 
-def get_random_picture():
-    return True
+def get_random_picture(request):
+    pic = Meme.objects.get(id=1)
+    html = "<html><body>It is now %s.</body></html>" % pic.meme_picture
+    return HttpResponse(html)
