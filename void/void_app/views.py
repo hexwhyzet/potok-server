332fa0c8600109dd1230ecf8a952746ed97b06f8
Alex <alexnossofff@gmail.com>
2020-04-18 22:45:39 +0300

    1) Added simple feed page 2) Added media settings in settings.py 3) Updated urlpatterns in urls.py 4) Updated get_random_picture func

--- void/void_app/views.py
+++ void/void_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .models import Meme
 from django.http import HttpResponse
+from django.template import loader
+from random import randint
 
 
 def get_random_picture(request):
-    with open(str(Meme.objects.get(id=3).meme_picture), "rb") as f:
-        return HttpResponse(f.read(), content_type="image/jpg")
+    mem = Meme.objects.get(id=randint(3, 4))
+    template = loader.get_template('void_app/feed.html')
+    context = {
+        'mem': mem,
+    }
+    return HttpResponse(template.render(context))
