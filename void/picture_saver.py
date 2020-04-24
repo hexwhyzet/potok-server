529e3b5f18dd11ddc019fe5d267d279cc569b96e
Alex <alexnossofff@gmail.com>
2020-04-24 23:24:52 +0300

    Added post_posts/ url to update main db from db from subproject

--- void/picture_saver.py
+++ void/picture_saver.py
@@ -14,18 +14,20 @@ def meme_json_parser(meme_data):
         picture_url = meme['photo']['link']
         pic_name = meme['post_id']
         pic_path = save_picture_from_link(picture_url, pic_name)
+        pic_path = pic_path[6:]
         Meme.objects.create_meme(meme, pic_path).save()
 
 
 def save_picture_from_link(url, pic_name):
     response = requests.get(url)
     if response.status_code == 200:
-        pic_path = 'saved_pics/' + str(pic_name) + '.png'
+        pic_path = 'media/saved_pics/' + str(pic_name) + '.png'
         with open(pic_path, 'wb') as out_file:
             out_file.write(response.content)
         return pic_path
 
 
-with open('D:\\ruthless-void\\void\\void_app\\meme_data_json.json', 'r') as json_cont:
-    cont = json_cont.read()
-    meme_json_parser(cont)
+if __name__ == '__main__':
+    with open('D:\\ruthless-void\\void\\void_app\\meme_data_json.json', 'r') as json_cont:
+        cont = json_cont.read()
+        meme_json_parser(cont)
--- void/void_app/views.py
+++ void/void_app/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse
 from django.template import loader
 from random import randint
 from django.http import JsonResponse
+import sys
+sys.path.insert(0, 'D:\\ruthless-void\\void')
+
+from picture_saver import meme_json_parser
 
 
 def get_random_picture(request):
@@ -21,4 +25,11 @@ def get_random_object_by_type(object_type):
 
 def add_like_to_meme(request, meme_id):
     Meme.objects.all().filter(id=meme_id).first().add_like()
+    return JsonResponse({'status': 'ok'})
+
+
+def update_memes_db(request, post_json_info):
+    with open('lellol.txt', 'w') as outfile:
+        print(str(post_json_info), file=outfile)
+    meme_json_parser(post_json_info)
     return JsonResponse({'status': 'ok'})
