b04651a127509ef032c9529ca22859fa8af155f2
Vanya Kabakov <hexwhyzet@gmail.com>
2020-05-24 15:10:46 +0300

    Added key generator

---
+++ void/void_app/functions.py
@@ -0,0 +1,12 @@
+from random import choices
+from string import ascii_lowercase, ascii_uppercase, digits
+
+
+def id_gen(length):
+    pool = ascii_lowercase + ascii_uppercase + digits
+    generated_id = "".join(choices(pool, k=length))
+    return generated_id
+
+
+if __name__ == '__main__':
+    print(id_gen(6))
